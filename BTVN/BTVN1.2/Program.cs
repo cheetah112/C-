35c245b4b84e0d90704ee5c50524595c65867976
cheetah112 <nonpvp123@gmail.com>
2023-05-15 12:56:07 +0800

    ok

---
+++ BTVN/BTVN1.2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace BTVN11
+{
+    class Program{
+        static void Main(String[] args)
+        {
+            float width;
+            float height;
+            Console.Write("Enter width: ");
+            width = float.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            height = float.Parse(Console.ReadLine());
+            float area = width * height;
+            Console.WriteLine("Area is: " + area);
+        }
+    }
+}
