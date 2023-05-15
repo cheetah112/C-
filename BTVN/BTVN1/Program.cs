66d788eb408cff304d2e8e35245aff65ab5214bd
cheetah112 <nonpvp123@gmail.com>
2023-05-15 12:37:39 +0800

    Initial commit

---
+++ BTVN/BTVN1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace SystemTime
+{
+         public class Program
+        {
+            static void Main(String[] args)
+            {
+                DateTime localDate = DateTime.Now;
+                System.Console.WriteLine("Datetime Now is: " + localDate);
+                System.Console.WriteLine("Datetime Now new format is: " + String.Format("{0: dd/MM/yyyy HH:mm:ss}", localDate));
+            }
+        }
+}
