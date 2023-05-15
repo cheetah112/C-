1801c3fa9f606d2f512fb68b11951d1c647643f5
cheetah112 <nonpvp123@gmail.com>
2023-05-15 13:15:05 +0800

    ok

---
+++ BTVN/BTVN1.3/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace BTVN13
+{
+    class program 
+    {
+        static void Main(String[] args)
+        {
+            Console.Write("Enter your name: ");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Hello " + yourName +" !");
+        }
+    }
+}
