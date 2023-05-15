9efa463570a8458f03981eba38d1f0e349fc3708
cheetah112 <nonpvp123@gmail.com>
2023-05-15 13:30:34 +0800

    ok

---
+++ BTVN/BTVN1.4/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace BTVN14
+{
+    class program 
+    {
+        static void Main(String[] args)
+        {
+            const int rate = 23000;
+            Console.Write("Enter your USD need to tranfer: ");
+            int usd = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your VND received: " + usd*rate);
+        }
+    }
+}
