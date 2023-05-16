c93b30ac7dbfa122585242e9418b6aced069d6c9
cheetah112 <nonpvp123@gmail.com>
2023-05-16 10:57:24 +0800

    ok

--- BTVN/BTVN2/Program.cs
+++ BTVN/BTVN2/Program.cs
@@ -7,14 +7,14 @@ namespace BTVN2
     {
         static void Main(string[] args)
         {
-            
-            Console.Write("Input your birthDay(dd/MM): ");
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.Write("Nhập ngày tháng sinh nhật của bạn(dd/MM): ");
             DateTime birthDay = DateTime.ParseExact(Console.ReadLine(),"dd/MM",null);
             DateTime toDay = DateTime.Today;
             DateTime nextYearBirthDay = new DateTime(toDay.Year+1,birthDay.Month,birthDay.Day);
             DateTime thisyearBirthDay = new DateTime(toDay.Year,birthDay.Month,birthDay.Day);
-            Console.WriteLine(toDay);
-            Console.WriteLine(((nextYearBirthDay-toDay).Days)%((nextYearBirthDay-thisyearBirthDay).Days));
+            Console.WriteLine( "Số ngày còn lại đến ngày sinh nhật của bạn là:" + ((nextYearBirthDay-toDay).Days)%((nextYearBirthDay-thisyearBirthDay).Days));
         }
     }
 }
