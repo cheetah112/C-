dca9159f4c78360c7e2803786e0512372626a03b
cheetah112 <nonpvp123@gmail.com>
2023-05-29 19:16:43 +0800

    ok

---
+++ b5/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace b5
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Nhap x:");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Nhap y:");
+            int y = int.Parse(Console.ReadLine());
+            swap(ref x, ref y);
+            Console.WriteLine("x:"+ x +" y:" + y);
+        }
+        private static void swap(ref int x, ref int y){
+            int t = x;
+            x = y;
+            y = t;
+        }
+    }
+}
